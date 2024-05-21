b413b3e0430c4ad7439b0d01a84b63d5b7e18ef2
ChemiLhamo <158521550+ChemiLhamo@users.noreply.github.com>
2024-05-21 21:10:20 +0000

    attempted making buttons work ...

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Declare variables below to save the different divs of your story.
 
-
-
-
-
-
-
+let storyOpening=document.querySelector(".story-opening");  
+let optionOne=document.querySelector(".option-one-screen");
+let optiontwo=document.querySelector(".option-two-screen");
+let optiononeend=document.querySelector(".option-one-end");
+let optiontwoend=document.querySelector(".option-two-end");
 
 
 // When you're ready to make event handlers, uncomment the code below. 
